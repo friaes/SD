eac3b9c4260ca5c68982ebc9052fd51f76ad6dfc
João Fidalgo <joao.fidalgo.1403@tecnico.ulisboa.pt>
2024-03-15 14:59:27 +0000

    Final version with 1 and 2 servers

---
+++ ServerR1/src/main/java/pt/ulisboa/tecnico/tuplespaces/server/ServerMain.java
@@ -0,0 +1,102 @@
+package pt.ulisboa.tecnico.tuplespaces.server;
+
+import io.grpc.*;
+
+import java.io.IOException;
+import java.util.List;
+import pt.ulisboa.tecnico.nameServer.contract.*;
+
+import static io.grpc.Status.INVALID_ARGUMENT;
+
+public class ServerMain {
+
+    /** Set flag to true to print debug messages. 
+	   * The flag can be set using the -Ddebug command line option. */
+	  private static boolean DEBUG_FLAG = false;
+	  private static ManagedChannel channelDNS = null;
+      private static NameServerServiceGrpc.NameServerServiceBlockingStub stubDNS = null;
+	  private static String targetDNS = "localhost:5001";
+
+	  /** Helper method to print debug messages. */
+	  private static void debug(String debugMessage) {
+		  if (DEBUG_FLAG)
+			  System.err.print("[DEBUG] " + debugMessage);
+	  }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println(ServerMain.class.getSimpleName());
+
+		if ((args.length == 3) && args[2].equals("-debug"))
+			DEBUG_FLAG = true;
+
+		// receive and print arguments
+		debug(String.format("Received %d arguments", args.length));
+		for (int i = 0; i < args.length; i++) {
+			debug(String.format("arg[%d] = %s", i, args[i]));
+		}
+
+		// check arguments
+		if (args.length < 1) {
+			debug("Argument(s) missing!");
+			debug(String.format("Usage: java %s port%n", ServerMain.class.getName()));
+			return;
+		}
+
+		final int port;
+		if (args.length >= 2) {
+			port = Integer.parseInt(args[0]);
+		}
+		else {
+			port = 2001;
+		}
+
+		final BindableService impl = new ServiceImpl(DEBUG_FLAG);
+		debug("Port: " + port);
+
+		// Create a new server to listen on port
+		Server server = ServerBuilder.forPort(port).addService(impl).build();
+
+		// Start the server
+		server.start();
+
+		Runtime.getRuntime().addShutdownHook(new Thread(()-> {
+			deleteDNS("TupleSpaces", "localhost:" + port);
+			channelDNS.shutdownNow();
+		}));
+
+
+		// Server threads are running in the background.
+		System.out.println("Server started");
+
+		
+		// Register in DNS
+		channelDNS = ManagedChannelBuilder.forTarget(targetDNS).usePlaintext().build();
+        stubDNS = NameServerServiceGrpc.newBlockingStub(channelDNS);
+		registerDNS("A", "TupleSpaces", "localhost:" + port);
+
+		// Do not exit the main thread. Wait until server is terminated.
+		server.awaitTermination();
+		
+    }
+
+	public static void registerDNS(String qualifier, String service, String address){
+		try {
+			NameServer.RegisterRequest request = NameServer.RegisterRequest.newBuilder().setQualifier(qualifier).setService(service).setAddress(address).build();
+			stubDNS.register(request);
+		}
+		catch (StatusRuntimeException e) {
+			System.out.println("Caught Exception with description: " + e.getStatus().getDescription());
+		}
+    }
+
+	public static void deleteDNS(String service, String address){
+		try {
+			NameServer.DeleteRequest request = NameServer.DeleteRequest.newBuilder().setService(service).setAddress(address).build();
+			stubDNS.delete(request);
+		}
+		catch (StatusRuntimeException e) {
+			System.out.println("Caught Exception with description: " + e.getStatus().getDescription());
+		}
+    }
+}
+
---
+++ ServerR1/src/main/java/pt/ulisboa/tecnico/tuplespaces/server/domain/ServerState.java
@@ -0,0 +1,62 @@
+package pt.ulisboa.tecnico.tuplespaces.server.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerState {
+
+  private List<String> tuples;
+
+  public ServerState() {
+    this.tuples = new ArrayList<String>();
+
+  }
+
+  public synchronized void put(String tuple) {
+    tuples.add(tuple);
+    notifyAll();
+  }
+
+  private synchronized String getMatchingTuple(String pattern) {
+    for (String tuple : this.tuples) {
+      if (tuple.matches(pattern)) {
+        return tuple;
+      }
+    }
+    return null;
+  }
+
+  public synchronized String read(String pattern) {
+    String tuple = getMatchingTuple(pattern);
+    
+    while (tuple == null){
+      try {
+        wait(); // wait until the tuple is inserted
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      tuple = getMatchingTuple(pattern);
+    }
+
+    return tuple;
+  }
+
+  public synchronized String take(String pattern) {
+    String tuple = getMatchingTuple(pattern);
+    while (tuple == null){
+      try {
+        wait(); // wait until the tuple is inserted
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      tuple = getMatchingTuple(pattern);
+    }
+    tuples.remove(tuple);
+    return tuple;
+  }
+
+  public synchronized List<String> getTupleSpacesState() {
+    return this.tuples;
+  }
+
+}
